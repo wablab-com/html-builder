cb2dd433bc4d792c0801256dbbcc21b6ca21665a
Ahmad Saleh <ahmadfds@gmail.com>
2021-02-27 10:40:31 +0300

    unit tests

---
+++ src/HTML/Attribute/Trait/NotSupportedInHtml5AttributesTrait.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace WabLab\HtmlBuilder\HTML\Attribute\Trait;
+
+trait NotSupportedInHtml5AttributesTrait {
+
+    use AlignAttributeTrait;
+    use BgcolorAttributeTrait;
+    use BorderAttributeTrait;
+    use ColorAttributeTrait;
+}
